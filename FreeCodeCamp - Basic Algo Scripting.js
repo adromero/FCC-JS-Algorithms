5e7db04522ccc7852c5b3f75708b52bdbfc6d25f
A R <adromero@users.noreply.github.com>
2018-04-24 13:31:39 -0400

    Added Slasher Flick

--- FreeCodeCamp - Basic Algo Scripting.js
+++ FreeCodeCamp - Basic Algo Scripting.js
@@ -183,3 +183,13 @@ chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 //------------------------------------------------------------------------------------------------
 
+//Slasher Flick 
+//Return the remaining elements of an array after chopping off n elements from the head.
+
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);
+
+//------------------------------------------------------------------------------------------------
