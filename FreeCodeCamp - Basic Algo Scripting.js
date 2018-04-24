ea3b77bbd3b45ae3a7dd5b3e48d9a915e295638a
A R <adromero@users.noreply.github.com>
2018-04-24 11:34:43 -0400

    Add files via upload

--- FreeCodeCamp - Basic Algo Scripting.js
+++ FreeCodeCamp - Basic Algo Scripting.js
@@ -148,4 +148,19 @@ repeatStringNumTimes("abc", 3);
 
 //------------------------------------------------------------------------------------------------
 
+//Truncate a string
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
 
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length <= num) {
+    return str;
+  } else if (num > 3){
+      return str.slice(0,num-3) + "...";
+  } else {
+    return str.slice(0,num) + "...";
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
